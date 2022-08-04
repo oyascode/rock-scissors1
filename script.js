265a3ac6187e711e05d4eacb1e8239f71076a6da
Michael John <johnmikeloy@gmail.com>
2022-08-04 14:40:24 -0400

    Feature to play 1 round added

--- script.js
+++ script.js
@@ -1,7 +1,20 @@
-const getComputerChoice = (arr) => {
+function getComputerChoice(arr){
   if (arr && arr.length) {
     return arr[Math.floor(Math.random() * arr.length)]
   }
 }
 const choiceArr = ["Rock", "Paper", "Scissors"];
-console.log(getComputerChoice(choiceArr))
+// console.log(getComputerChoice(choiceArr))
+
+function playRound(playerSelection, computerSelection) {
+  if (playerSelection === /"Rock"/i && computerSelection === "Paper") {
+    return "You Lose!, paper beats Rock"
+  }
+  return "Try again"
+} 
+
+
+const playerSelection = "rock";
+const computerSelection = getComputerChoice(choiceArr);
+console.log(computerSelection)
+console.log(playRound(playerSelection, computerSelection));
